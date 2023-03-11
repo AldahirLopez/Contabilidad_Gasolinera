fbb6f83947b9d3bdaa08f75408c0fbd6321a2276
Aldahir Lopez Mac <hectoralr21@gmail.com>
2023-03-11 13:29:10 -0600

    Cambios vista empleado

--- resources/views/empleados/crear.blade.php
+++ resources/views/empleados/crear.blade.php
@@ -1,54 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="section">
-        <div class="section-header">
-            <h3 class="page__heading">Registrar Empleado</h3>
-        </div>
-        <div class="section-body">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-body">     
-                                                                      
-                        @if ($errors->any())                                                
-                            <div class="alert alert-dark alert-dismissible fade show" role="alert">
-                            <strong>¡Revise los campos!</strong>                        
-                                @foreach ($errors->all() as $error)                                    
-                                    <span class="badge badge-danger">{{ $error }}</span>
-                                @endforeach                        
+<section class="section">
+    <div class="section-header">
+        <h3 class="page__heading">Registrar Empleado</h3>
+    </div>
+    <div class="section-body">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+
+                        @if ($errors->any())
+                        <div class="alert alert-dark alert-dismissible fade show" role="alert">
+                            <strong>¡Revise los campos!</strong>
+                            @foreach ($errors->all() as $error)
+                            <span class="badge badge-danger">{{ $error }}</span>
+                            @endforeach
                             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
-                            </div>
+                        </div>
                         @endif
 
-                    <form action="{{ route('empleados.store') }}" method="POST">
-                        @csrf
+                        <form action="{{ route('empleados.store') }}" method="POST">
+                            @csrf
 
-                           <div class="row">
-                            <div class="col-xs-12 col-sm-12 col-md-12">
-                                <div class="form-group">
-                                   <label for="Nombre_Emp">Nombre</label>
-                                   <input type="text" name="Nombre_Emp" class="form-control">
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <label for="Nombre_Emp">Nombre</label>
+                                        <input type="text" name="Nombre_Emp" class="form-control">
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div class="col-xs-12 col-sm-12 col-md-12">
-                                <div class="form-group">
-                                   <label for="Ap_Paterno_Emp">Apellido Paterno</label>
-                                   <input type="text" name="Ap_Paterno_Emp" class="form-control">
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <label for="Ap_Paterno_Emp">Apellido Paterno</label>
+                                        <input type="text" name="Ap_Paterno_Emp" class="form-control">
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div class="col-xs-12 col-sm-12 col-md-12">
-                                <div class="form-group">
-                                   <label for="Ap_Materno_Emp">Apellido Materno</label>
-                                   <input type="text" name="Ap_Materno_Emp" class="form-control">
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <label for="Ap_Materno_Emp">Apellido Materno</label>
+                                        <input type="text" name="Ap_Materno_Emp" class="form-control">
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="col-xs-12 col-sm-12 col-md-12">
                                     <div class="form-group">
                                         <label form="Id_Turno">Turno</label>
                                         <select class="form-control" name="Id_Turno">
@@ -57,19 +57,16 @@
                                             @endforeach
                                         </select>
                                     </div>
+                                </div>
                             </div>
-                            </div>
-                            
-                            
-                            <button type="submit" class="btn btn-primary">Guardar</button>                            
-                         </div>
-                        </div>
-                    </form>
-                    
-                        </div>
+                        <button type="submit" class="btn btn-primary">Guardar</button>
                     </div>
                 </div>
+             </form>
             </div>
         </div>
-    </section>
+    </div>
+    </div>
+    </div>
+</section>
 @endsection
